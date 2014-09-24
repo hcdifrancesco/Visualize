c3204f5bd6aa7dc955a59abec3d0d3a18eb0547c
Harry DiFrancesco <hcdifrancesco@gmail.com>
2014-09-24 16:34:11 -0600

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-   config.action_mailer.default_url_options = { :host 'tovisualize.herokuapp.com }
+   config.action_mailer.default_url_options = { host: 'tovisualize.herokuapp.com' } 
   #Required for Heroku
   #Note to set this to your actual host.
 end
